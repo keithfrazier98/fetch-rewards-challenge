f5754b95c959f4b681088bfd4201028169a3c6b6
Keith Frazier <keithers98@gmail.com>
2021-09-07 14:26:52 -0700

    moved db dir points dir, app.js, and server.js into src directory, created app.js file and server.js file, setup basic local server for development, configured knex file with postgre sql (elephant sql) database

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,19 @@
-// Update with your config settings.
+require('dotenv').config()
+const path = require("path")
+
+const {DATABASE_URL} = process.env
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    pool:{min:1, max:5},
+    connection: DATABASE_URL,
+    migrations:{
+      directory:path.join(__dirname, "src", "db", "migrations")
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds:{
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
-
 };
